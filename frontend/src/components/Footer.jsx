311296665a3779ac5d5ecfaed021e9de25ed6731
Firas <firasdabbabi@gmail.com>
2023-05-26 22:17:24 +0100

    version 1.7: insert footer @Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
 
 export default function Footer() {
     return (
         <div className="w-full bg-[#0c2461] text-gray-200 py-8 px-2">
-            footer section
+            <div className="max-w-[1240px] mx-auto grid grid-cols-2 md:grid-cols-6 border-b-2 border-gray-600">
+
+
+            </div>
         </div>
     )
 }
