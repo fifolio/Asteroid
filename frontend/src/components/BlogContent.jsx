805485c7f42592a5ccf0b71084a0b898a022c8e7
Firas <firasdabbabi@gmail.com>
2023-05-31 19:48:01 +0100

    version 4: liking the Author name

--- frontend/src/components/BlogContent.jsx
+++ frontend/src/components/BlogContent.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
 import { ReactMarkdown } from 'react-markdown/lib/react-markdown'
+import { Link } from 'react-router-dom'
 
 export default function BlogContent({ articles }) {
 
@@ -31,7 +32,9 @@ export default function BlogContent({ articles }) {
 
                     <div className="bg-white w-full h-max drop-shadow-md rounded-xl py-5 px-4 max-sm:col-span-2 max-sm:mt-8">
                         <img src={`http://localhost:1337${article.attributes.authorImg.data.attributes.url}`} alt="" className='p-2 w-40 h-40 rounded-full mx-auto' />
-                        <h1 className="font-bold text-2xl text-center text-gray-900 pt-3 mb-4">{article.attributes.authorName}</h1>
+                        <Link to="/author">
+                            <h1 className="font-bold text-2xl text-center text-gray-900 pt-3 mb-4">{article.attributes.authorName}</h1>
+                        </Link>
                         <p className="text-center text-gray-900 font-medium">{article.attributes.authorDesc}</p>
                     </div>
 
