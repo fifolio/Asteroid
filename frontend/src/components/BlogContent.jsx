be7f9a359013eb7a48169269a104da9f8e1706f0
Firas <firasdabbabi@gmail.com>
2023-05-31 19:45:21 +0100

    version 3.9: fixing the Author size box

--- frontend/src/components/BlogContent.jsx
+++ frontend/src/components/BlogContent.jsx
@@ -29,9 +29,9 @@ export default function BlogContent({ articles }) {
                         </ReactMarkdown>
                     </div>
 
-                    <div className="bg-white w-full drop-shadow-md rounded-xl py-5 max-sm:col-span-2 max-sm:mt-8 h-[280px]">
+                    <div className="bg-white w-full h-max drop-shadow-md rounded-xl py-5 px-4 max-sm:col-span-2 max-sm:mt-8">
                         <img src={`http://localhost:1337${article.attributes.authorImg.data.attributes.url}`} alt="" className='p-2 w-40 h-40 rounded-full mx-auto' />
-                        <h1 className="font-bold text-2xl text-center text-gray-900 pt-3">{article.attributes.authorName}</h1>
+                        <h1 className="font-bold text-2xl text-center text-gray-900 pt-3 mb-4">{article.attributes.authorName}</h1>
                         <p className="text-center text-gray-900 font-medium">{article.attributes.authorDesc}</p>
                     </div>
 
