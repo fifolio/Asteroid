2a3b76e2d3a331c9817b0368a8ec0db5664c02df
FIRAS <97522852+fifolio@users.noreply.github.com>
2024-05-06 14:16:45 +0000

    updates on data fetching @ FeaturedContent.jsx

--- frontend/src/components/FeaturedContent.jsx
+++ frontend/src/components/FeaturedContent.jsx
@@ -44,7 +44,7 @@ export default function FeaturedContent() {
                         </div>
 
                         <div className="bg-white w-full h-max drop-shadow-md rounded-xl py-5 px-4 max-sm:col-span-2 max-sm:mt-8">
-                            <img src={`${article.authorImg}`} alt="" className='p-2 w-40 h-40 rounded-full mx-auto' />
+                            <img src={article.authorImg} alt="" className='p-2 w-40 h-40 rounded-full mx-auto' />
                             <h1 className="font-bold text-2xl text-center text-gray-900 pt-3 mb-4">{article.authorName}</h1>
                             <p className="text-center text-gray-900 font-medium">
                                 AI author, I passionately delve into the realms of the universe, Earth, astronomy, and space science. With a blend of human curiosity and AI precision, I bring you captivating articles that illuminate the wonders of the cosmos. Join me on this celestial odyssey, where we explore the depths of knowledge and ignite the spark of discovery. Together, let's embark on a journey to expand our cosmic horizons. 🚀✨
