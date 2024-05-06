88ea9783f9dcf6e15fbe87a908932faf60c7ed99
FIRAS <97522852+fifolio@users.noreply.github.com>
2024-05-06 17:22:53 +0000

    .

--- frontend/src/pages/ArticlesPage.jsx
+++ frontend/src/pages/ArticlesPage.jsx
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom'
 
 export default function ArticlesPage({ articlesData }) {
 
-    const [articles, setArticles] = useState({})
-    setArticles(articlesData)
-    console.log(articles)
+    // const [articles, setArticles] = useState({})
+    // setArticles(articlesData)
+    console.log(articlesData)
 
     const handleLinkClick = () => {
         window.scrollTo(0, 0);
@@ -21,7 +21,7 @@ export default function ArticlesPage({ articlesData }) {
                 <div className="max-w-[1240px] mx-auto mt-[80px]">
                     <div className="grid lg:grid-cols-3 md:grid-cols-2 gap-4 px-4 text-black">
 
-                        {articles.map((article) => (
+                        {/* {articles.map((article) => (
                             <Link key={article.$id} to={`/article/${article.$id}`} onClick={() => handleLinkClick()}>
                                 <div className="bg-white rounded-sm overflow-hidden drop-shadow-md lg:h-[450px] md:h-[450px]">
                                     <img src={`${article.coverImg}`} className="h-56 w-full object-cover" />
@@ -36,7 +36,7 @@ export default function ArticlesPage({ articlesData }) {
                                     </div>
                                 </div>
                             </Link>
-                        ))}
+                        ))} */}
 
                     </div>
 
