e42eefe356ab73eddc02f35e1a9b99a39eaae485
FIRAS <97522852+fifolio@users.noreply.github.com>
2024-05-07 11:06:49 +0000

    .

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,3 @@
-import { appwriteConfig, databases } from '../appwrite/config';
-import { useEffect, useState } from 'react';
 import { BlogContentPage, Homepage, AboutPage, ArticlesPage, AuthorPage, HotPage, FeaturedPage } from "./pages/index"
 import { Routes, Route } from 'react-router-dom';
 // import LoadingPage from "./pages/LoadingPage";
@@ -13,39 +11,16 @@ export default function App() {
   // if (loading) return (<LoadingPage />, <Countdown />)
   // if (error) return <p>Error!</p>
 
-  const [data, setData] = useState(null)
-
-  useEffect(() => {
-
-    const fetchData = async () => {
-        // setLoading(true)
-        try {
-            const res =await databases.listDocuments(
-                appwriteConfig.database, 
-                appwriteConfig.collection_blog
-            );
-            setData(res.documents)
-            console.log('App: ', res.documents)
-            // setLoading(false)
-        } catch (error) {
-            setError(error)
-            // setLoading(false)
-        }
-    }
-
-    fetchData()
-
-}, [])
-
+ 
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path='/' element={<Homepage articles={data} />} />
+        <Route path='/' element={<Homepage />} />
         <Route path="*" element={<ErrorPage />} />
         <Route path='/featured/:id' element={<FeaturedPage />} />
         <Route path='/hot' element={<HotPage />} />
-        {/* <Route path="/articles" element={<ArticlesPage articles={data} />} /> */}
+        <Route path="/articles" element={<ArticlesPage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/author" element={<AuthorPage />} />
         <Route path='/article/:id' element={<BlogContentPage />} />
--- frontend/src/pages/ArticlesPage.jsx
+++ frontend/src/pages/ArticlesPage.jsx
@@ -1,12 +1,26 @@
-import { useState } from "react";
-import { Footer, Navbar } from "../components"
+import { appwriteConfig, databases } from "../../appwrite/config";
+import { useEffect, useState } from "react";
 import { Link } from 'react-router-dom'
 
-export default function ArticlesPage({ articlesData }) {
+export default function ArticlesPage() {
 
-    // const [articles, setArticles] = useState({})
-    // setArticles(articlesData)
-    console.log(articlesData)
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await databases.listDocuments(
+                    appwriteConfig.database,
+                    appwriteConfig.collection_blog,
+                );
+                setData(response.documents)
+            } catch (error) {
+                console.error('something went wrong while fetching: ', error);
+            }
+        }
+
+        fetchData();
+    }, []);
 
     const handleLinkClick = () => {
         window.scrollTo(0, 0);
@@ -21,7 +35,7 @@ export default function ArticlesPage({ articlesData }) {
                 <div className="max-w-[1240px] mx-auto mt-[80px]">
                     <div className="grid lg:grid-cols-3 md:grid-cols-2 gap-4 px-4 text-black">
 
-                        {/* {articles.map((article) => (
+                        {data.map((article) => (
                             <Link key={article.$id} to={`/article/${article.$id}`} onClick={() => handleLinkClick()}>
                                 <div className="bg-white rounded-sm overflow-hidden drop-shadow-md lg:h-[450px] md:h-[450px]">
                                     <img src={`${article.coverImg}`} className="h-56 w-full object-cover" />
@@ -36,7 +50,7 @@ export default function ArticlesPage({ articlesData }) {
                                     </div>
                                 </div>
                             </Link>
-                        ))} */}
+                        ))}
 
                     </div>
 
