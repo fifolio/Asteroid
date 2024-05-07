ab814c8465e9da6a2f6d2cc81dfd11e3191132a1
FIRAS <97522852+fifolio@users.noreply.github.com>
2024-05-07 10:59:03 +0000

    .

--- frontend/src/components/BlogContent.jsx
+++ frontend/src/components/BlogContent.jsx
@@ -2,6 +2,7 @@ import { appwriteConfig, databases } from '../../appwrite/config';
 import { useParams } from 'react-router-dom'
 import { ReactMarkdown } from 'react-markdown/lib/react-markdown'
 import { Link } from 'react-router-dom'
+import { useState, useEffect } from 'react';
 
 export default function BlogContent() {
 
