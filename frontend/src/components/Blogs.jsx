191b2d9920aa5999a7c34e6db10a6be9a45d890a
FIRAS <97522852+fifolio@users.noreply.github.com>
2024-05-07 10:48:58 +0000

    .

--- frontend/src/components/Blogs.jsx
+++ frontend/src/components/Blogs.jsx
@@ -1,5 +1,5 @@
 import { appwriteConfig, databases } from '../../appwrite/config';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom'
 
 export default function Blogs() {
